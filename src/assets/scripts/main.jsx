fef461835c32bf83ff440e9b89c27eb5a9ee0276
ScarWu <xneriscool@gmail.com>
2018-10-21 00:52:43 +0800

    1. update assets

--- src/assets/scripts/main.jsx
+++ src/assets/scripts/main.jsx
@@ -1,7 +1,22 @@
 'use strict';
+/**
+ * Application Bootstrap
+ *
+ * @package     NodeJS API Viewer
+ * @author      Scar Wu
+ * @copyright   Copyright (c) Scar Wu (http://scar.tw)
+ * @link        https://github.com/scarwu/NodeAPIViewer
+ */
 
-var api_url = 'https://nodejs.org/api/';
-var api_type = {
+// Load Libraries
+import $ from 'jquery';
+
+String.prototype.repeat = (times) => {
+    return (new Array(times + 1)).join(this);
+};
+
+let apiHost = 'https://nodejs.org/api/';
+let contentType = {
     miscs: true,
     globals: true,
     methods: true,
@@ -13,27 +28,22 @@ var api_type = {
     options: true
 };
 
-function getAPIData (target, cb) {
-    if (target in window.localStorage) {
-        var data = JSON.parse(window.localStorage[target]);
-        cb && cb(data);
-    }
-    else {
-        $.getJSON(api_url + target + '.json', function(data) {
-            window.localStorage[target] = JSON.stringify(data);
-            cb && cb(data);
+function fetchData (contentName, callback) {
+    if (contentName in window.localStorage) {
+        callback && callback(JSON.parse(window.localStorage[contentName]));
+    } else {
+        $.getJSON(apiHost + contentName + '.json', (data) => {
+            window.localStorage[contentName] = JSON.stringify(data);
+
+            callback && callback(data);
         });
     }
 }
 
-function printContent (data, level, cb) {
-    String.prototype.repeat = function(times) {
-        return (new Array(times + 1)).join(this);
-    };
-
+function printContent (data, level, callback) {
     for (var index in data) {
-        if (index in api_type) {
-            for(var order in data[index]) {
+        if (index in contentType) {
+            for (var order in data[index]) {
                 var current = data[index][order];
 
                 var div = $('<div>').attr('class', 'block');
@@ -43,16 +53,17 @@ function printContent (data, level, cb) {
                 div.append(header);
 
                 var desc = $('<div>');
+
                 if ('stability' in current) {
                     var stability = $('<pre>').html('<code>Stability ' + current.stability +
                         ': ' + current.stabilityText + '</code>');
                     desc.append(stability);
                 }
+
                 desc.append(current.desc);
                 div.append(desc);
 
                 $('#content').append(div);
-
 
                 var item = $('<span>').html('&nbsp;'.repeat((level - 1) * 8) + current.textRaw.replace('\\', ''));
                 $('#item').append(item);
@@ -62,33 +73,35 @@ function printContent (data, level, cb) {
         }
     }
 
-    cb && cb();
+    callback && callback();
 }
 
 function selectItem () {
-    for (var index = $('#content .block').size()-1;index >= 0;index--) {
-        var current = $('#content .block').eq(index);
-        if(current.position().top <= 1) {
+    for (var index = $('#content .block').size()-1; index >= 0; index--) {
+        if ($('#content .block').eq(index).position().top <= 1) {
             $('#item span').eq(index).addClass('active_b').siblings().removeClass('active_b');
-            return;
+
+            break;
         }
     }
 }
 
 function contentResize () {
     if ($('#content .block').last().height() < $('#content').height()) {
-        var content = $('#content .block').last().css({
+        $('#content .block').last().css({
             height: $('#content').height() - 30
         });
     }
 }
 
-$(document).ready(function () {
-    getAPIData('index', function (data) {
-        $.each(data.desc, function (index) {
+$(document).ready(() => {
+    fetchData('index', (data) => {
+        $.each(data.desc, (index) => {
             var item = data.desc[index];
+
             if ('text' == item.type) {
                 var text = item.text.match(/\[(.+)\]\((.+)\.html\)/);
+
                 $('#nav').append('<span class="' + text[2] + '">' + text[1] + '</span>');
             }
         });
@@ -97,12 +110,13 @@ $(document).ready(function () {
     });
 });
 
-$('body').delegate('#clear', 'click', function () {
-    for (var index in window.localStorage)
+$('body').delegate('#clear', 'click', () => {
+    for (var index in window.localStorage) {
         window.localStorage.removeItem(index);
-    
+    }
+
     location.reload();
-}).delegate('#nav span', 'click', function () {
+}).delegate('#nav span', 'click', () => {
     $(this).addClass('active_a').siblings().removeClass('active_a');
 
     $('#item').html('');
@@ -111,17 +125,17 @@ $('body').delegate('#clear', 'click', function () {
         scrollTop: 0
     }, 750);
 
-    getAPIData($(this).attr('class').split(' ')[0], function (data) {
-        printContent(data, 1, function () {
+    fetchData($(this).attr('class').split(' ')[0], function (data) {
+        printContent(data, 1, () => {
             selectItem();
             contentResize();
 
-            for(var index = 0;index < $('#item span').size();index++) {
+            for (var index = 0; index < $('#item span').size(); index++) {
                 $('#item span').eq(index).attr('data-order', index);
             }
         });
     });
-}).delegate('#item span', 'click', function () {
+}).delegate('#item span', 'click', () => {
     var index = $(this).attr('data-order');
     var moveTo = $('#content .block').eq(index).position().top - $('#content .block').eq(0).position().top;
 
@@ -130,7 +144,7 @@ $('body').delegate('#clear', 'click', function () {
     }, 750);
 });
 
-$('#content').on('scroll', function () {
+$('#content').on('scroll', () => {
     selectItem();
     contentResize();
 });
