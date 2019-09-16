215c6e90b26629f0d86b51c3b0a4304459838c3b
ScarWu <xneriscool@gmail.com>
2019-09-16 15:03:21 +0800

    1. add vue dev tools

--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,86 @@
  * @link        https://github.com/scarwu/MHWCalculator
  */
 
-var path = require('path');
+const path = require('path');
+const webpackLodashPlugin = require('lodash-webpack-plugin');
+const webpackOptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
+const webpackMiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+function cssLoaders (options) {
+    options = options || {};
+
+    let cssLoader = {
+        loader: 'css-loader',
+        options: {
+            sourceMap: options.sourceMap
+        }
+    };
+
+    let postcssLoader = {
+        loader: 'postcss-loader',
+        options: {
+            sourceMap: options.sourceMap
+        }
+    };
+
+    // generate loader string to be used with extract text plugin
+    function generateLoaders (loader, loaderOptions) {
+        let loaders = options.usePostCSS
+            ? [cssLoader, postcssLoader]
+            : [cssLoader];
+
+        if (loader) {
+            loaders.push({
+                loader: loader + '-loader',
+                options: Object.assign({}, loaderOptions, {
+                    sourceMap: options.sourceMap
+                })
+            });
+        }
+
+        // Extract CSS when that option is specified
+        // (which is the case during production build)
+        return [webpackMiniCssExtractPlugin.loader].concat(loaders);
+        // if (options.extract) {
+        //     return [webpackMiniCssExtractPlugin.loader].concat(loaders);
+        // } else {
+        //     return ['vue-style-loader'].concat(loaders);
+        // }
+    }
+
+    // https://vue-loader.vuejs.org/en/configurations/extract-css.html
+    return {
+        css: generateLoaders(),
+        postcss: generateLoaders(),
+        less: generateLoaders('less'),
+        sass: generateLoaders('sass', { indentedSyntax: true }),
+        scss: generateLoaders('sass'),
+        stylus: generateLoaders('stylus'),
+        styl: generateLoaders('stylus')
+    };
+}
+
+// Generate loaders for standalone style files (outside of .vue)
+function styleLoaders (options) {
+    let output = [];
+    let loaders = cssLoaders(options);
+
+    for (let extension in loaders) {
+        let loader = loaders[extension];
+
+        output.push({
+            test: new RegExp('\\.' + extension + '$'),
+            use: loader
+        });
+    }
+
+    return output;
+}
 
 module.exports = {
     mode: 'development',
     entry: {
-        main: './src/assets/scripts/main.jsx'
+        main: './src/assets/scripts/main.js'
     },
     output: {
         filename: '[name].min.js'
@@ -25,7 +99,7 @@ module.exports = {
         ],
         extensions: [
             '.js',
-            '.jsx'
+            '.vue'
         ]
     },
     externals: {
@@ -34,45 +108,75 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /.jsx$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader',
-                        query: {
-                            cacheDirectory: true,
-                            plugins: [
-                                // Stage 0
-                                "@babel/plugin-proposal-function-bind",
-
-                                // Stage 1
-                                "@babel/plugin-proposal-export-default-from",
-                                "@babel/plugin-proposal-logical-assignment-operators",
-                                ["@babel/plugin-proposal-optional-chaining", { "loose": false }],
-                                ["@babel/plugin-proposal-pipeline-operator", { "proposal": "minimal" }],
-                                ["@babel/plugin-proposal-nullish-coalescing-operator", { "loose": false }],
-                                "@babel/plugin-proposal-do-expressions",
-
-                                // Stage 2
-                                ["@babel/plugin-proposal-decorators", { "legacy": true }],
-                                "@babel/plugin-proposal-function-sent",
-                                "@babel/plugin-proposal-export-namespace-from",
-                                "@babel/plugin-proposal-numeric-separator",
-                                "@babel/plugin-proposal-throw-expressions",
-
-                                // Stage 3
-                                "@babel/plugin-syntax-dynamic-import",
-                                "@babel/plugin-syntax-import-meta",
-                                ["@babel/plugin-proposal-class-properties", { "loose": false }],
-                                "@babel/plugin-proposal-json-strings"
-                            ],
-                            presets: [
-                                '@babel/preset-react'
-                            ]
-                        }
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: cssLoaders({
+                        sourceMap: (process.env.NODE_ENV !== 'production'),
+                        extract: (process.env.NODE_ENV === 'production')
+                    }),
+                    cssSourceMap: (process.env.NODE_ENV !== 'production'),
+                    cacheBusting: true,
+                    transformToRequire: {
+                        video: [ 'src', 'poster' ],
+                        source: 'src',
+                        img: 'src',
+                        image: 'xlink:href'
                     }
-                ]
+                }
+            },
+            ...(styleLoaders({
+                sourceMap: (process.env.NODE_ENV !== 'production'),
+                extract: (process.env.NODE_ENV === 'production'),
+                usePostCSS: true
+            })),
+            {
+                test: /\.js$/,
+                loader: (process.env.NODE_ENV !== 'production') ? '@sucrase/webpack-loader' : 'babel-loader?cacheDirectory=true',
+                options: (process.env.NODE_ENV !== 'production') ? { transforms: ['jsx'] } : {}
+            },
+            {
+                test: /\.pug$/,
+                loader: 'pug-plain-loader'
             }
+            // {
+            //     test: /.jsx$/,
+            //     exclude: /node_modules/,
+            //     use: {
+            //         loader: 'babel-loader',
+            //         options: {
+            //             cacheDirectory: true,
+            //             plugins: [
+            //                 // Stage 0
+            //                 "@babel/plugin-proposal-function-bind",
+
+            //                 // Stage 1
+            //                 "@babel/plugin-proposal-export-default-from",
+            //                 "@babel/plugin-proposal-logical-assignment-operators",
+            //                 ["@babel/plugin-proposal-optional-chaining", { "loose": false }],
+            //                 ["@babel/plugin-proposal-pipeline-operator", { "proposal": "minimal" }],
+            //                 ["@babel/plugin-proposal-nullish-coalescing-operator", { "loose": false }],
+            //                 "@babel/plugin-proposal-do-expressions",
+
+            //                 // Stage 2
+            //                 ["@babel/plugin-proposal-decorators", { "legacy": true }],
+            //                 "@babel/plugin-proposal-function-sent",
+            //                 "@babel/plugin-proposal-export-namespace-from",
+            //                 "@babel/plugin-proposal-numeric-separator",
+            //                 "@babel/plugin-proposal-throw-expressions",
+
+            //                 // Stage 3
+            //                 "@babel/plugin-syntax-dynamic-import",
+            //                 "@babel/plugin-syntax-import-meta",
+            //                 ["@babel/plugin-proposal-class-properties", { "loose": false }],
+            //                 "@babel/plugin-proposal-json-strings"
+            //             ],
+            //             presets: [
+            //                 '@babel/preset-env'
+            //             ]
+            //         }
+            //     }
+            // }
         ]
     }
 }
